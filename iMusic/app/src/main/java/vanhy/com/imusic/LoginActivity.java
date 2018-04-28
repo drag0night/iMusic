b6b8f7fe1c7f3161456a9a3392574c3718483268
quangvn <quangvn@gmail.com>
2018-04-28 08:56:37 +0700

    update

--- iMusic/app/src/main/java/vanhy/com/imusic/LoginActivity.java
+++ iMusic/app/src/main/java/vanhy/com/imusic/LoginActivity.java
@@ -2,12 +2,17 @@ package vanhy.com.imusic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class LoginActivity extends AppCompatActivity {
+
+    RelativeLayout login_layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login_layout = findViewById(R.id.login_layout);
+        login_layout.setBackgroundResource(R.drawable.background);
     }
 }
