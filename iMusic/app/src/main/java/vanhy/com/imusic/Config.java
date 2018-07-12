32c4751c56d3bd94d9f84d49af959d315b2e8554
drag0night <37505407+drag0night@users.noreply.github.com>
2018-07-12 22:02:01 +0700

    delete
    123123

--- iMusic/app/src/main/java/vanhy/com/imusic/Config.java
+++
@@ -1,9 +0,0 @@
-package vanhy.com.imusic;
-
-public class Config {
-    public static final String CLIENT_ID = "a7Ucuq0KY8Ksn8WzBG6wj4x6pcId6BpU";
-    public static final String COUNTRY = "country";
-    public static final String POP = "pop";
-    public static final String ROCK = "rock";
-    public static final String DANCEEDM = "danceedm";
-}
